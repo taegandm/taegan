40ae6951c84deeda85d7d8dec06b3c3bf3da8af3
Ryan <Ryanmcgrath@live.com>
2023-02-15 13:00:18 -0500

    inital commit

---
+++ src/enroll.php
@@ -0,0 +1,15 @@
+<?php
+
+$auth1 = $_POST['email'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>Hello from validate</h1>
+</body>
+</html>
