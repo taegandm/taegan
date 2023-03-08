95ef0291c9763657dc2b47c22f3c31ab285d7fe6
Ryan <Ryanmcgrath@live.com>
2023-03-08 11:08:47 -0500

    inital commit

--- src/enroll.php
+++ src/enroll.php
@@ -1,15 +1,49 @@
 <?php
+$username = $_POST['name'];
+$password = $_POST['password'];
+$account_type = $_POST['account_type'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
 
-$auth1 = $_POST['email'];
+//This would be expanded to accomdate for more roles
+if ($account_type == 'Customer') {
+    $type = 0;
+} else {
+    $type = 1;
+}
+
+
+$mysqli = new mysqli('localhost', 'mcgratr1', 'Thompson16!', 'mcgratr1_Test');
+
+//echo mysqli_connect_errno();
+
+if (mysqli_connect_errno()) {
+    echo mysqli_connect_error();
+    exit;
+}
+
+//ignore error
+$sql = "INSERT INTO auth_info (username, password, account_type, email, phone)
+VALUES ('$username', '$password', '$type', '$email', '$phone')";
+
+$result = $mysqli->query($sql);
+$mysqli->close();
+
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title></title>
 </head>
-<body>
-<h1>Hello from validate</h1>
+<body onload="returnHome()">
+
+<script>
+    function returnHome(){
+        window.location.href = "http://cyan.csam.montclair.edu/~mcgratr1/Final%20Project/src/";
+    }
+</script>
 </body>
 </html>
