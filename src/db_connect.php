d3c9864e2a0f77524245419283a1d0f5c443b820
Taegan <56590478+taegandm@users.noreply.github.com>
2023-04-12 18:26:03 -0400

    Connected "manage alert" to sql database
    Manage alert now uses db_connect.php and send_alert.php to connect to the database and insert the values into the sql table
    Possible errors:
    - hours is incorrect (wrong timezone?) when submitting to table in send_alert.php
    - might need to refactor reference paths to pages to make them connect

--- src/db_connect.php
+++ src/db_connect.php
@@ -4,7 +4,7 @@ function OpenCon()
 {
     $dbhost = "localhost";
     $dbuser = "root";
-    $dbpass = "password";
+    $dbpass = "4TaeMaish!";
     $db = "swe2";
 
     $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
