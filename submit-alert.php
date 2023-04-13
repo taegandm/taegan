fd7cc9f23dd53a7e7ff71eef406cc2fc4397e360
Taegan <56590478+taegandm@users.noreply.github.com>
2023-04-13 08:22:07 -0400

    Changed SQL in submit-alert
    Changed the SQL query in submit alert

---
+++ submit-alert.php
@@ -0,0 +1,22 @@
+<?php
+    // Include the database connection file
+    include __DIR__ . "\db_connect.php";
+    $conn = OpenCon();
+
+    // Get the values submitted from the modal
+    $title = $_POST['alertTitle'];
+    $details = $_POST['alertDetails'];
+    $dt = date("m\/d\/y h:i:s");
+
+    //create the sql statement to insert into the table
+    $sql = "INSERT INTO alerts (title, details, dt) 
+                VALUES ('$title', '$details', '$dt')";
+        
+    //query the sql statement
+    $result = mysqli_query($conn, $sql);
+
+    // Redirect back to the previous page
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+    CloseCon($conn);
+    exit();
+?>
